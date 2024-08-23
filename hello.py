790687905becf159fe4f3d206aaa5615a669629f
Maishe <117469918+Eliab-kamau@users.noreply.github.com>
2024-08-23 23:53:12 +0300

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,18 @@
+greetings="man !"
+print(greetings)
+print("hello there")
+sum= 4+5
+print(sum)
+print("python programming language")
+
+
+#inserting a break makes line move to the next line
+
+print("python is wonderful to learn\n Its the easiest to learn")
+
+a="Awesome"
+print("Python is"  +a)
+
+# comments in python
+# 1. # symbol for single line quote
+# 2.  ''' or """ triple double or single qoutes
